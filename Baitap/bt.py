e27cb5cd7925e0cb2bd6ed9eabbcef1f1435ddf6
Willmae <phuctran3289@gmail.com>
2018-10-20 23:38:00 +0700

    .

---
+++ Baitap/bt.py
@@ -0,0 +1,3 @@
+C = float(input("Enter the temperature in Celcius? "))
+F = C * 1.8 + 32 
+print(C, "(C)", " = ", F, "(F)")
---
+++ Baitap/bt2.py
@@ -0,0 +1,3 @@
+r = float(input("radius? "))
+A = r ** 2 * 3.14 
+print("Area = ", A)
